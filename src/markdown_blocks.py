e4ac27219a4c1cbe90b922aaa88c770bb64beaf5
kaszta1274 <mateusz.gancarczyk04@gmail.com>
2026-07-09 11:22:06 +0200

    add block to block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,14 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     result = []
@@ -6,3 +17,41 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         if block != "":
             result.append(block)
     return result
+
+def block_to_block_type(block: str) -> BlockType:
+    heading_matches = re.findall(r"^#{1,6} .+", block)
+    if heading_matches:
+        return BlockType.HEADING
+
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    
+    lines = block.split("\n")
+
+    quote = True
+    for line in lines:
+        if not line.startswith(">"):
+            quote = False
+            break
+    if quote:
+        return BlockType.QUOTE
+    
+    unordered = True
+    for line in lines:
+        if not line.startswith("- "):
+            unordered = False
+            break
+    if unordered:
+        return BlockType.UNORDERED_LIST
+    
+    ordered = True
+    i = 1
+    for line in lines:
+        if not line.startswith(f"{i}. "):
+            ordered = False
+            break
+        i += 1
+    if ordered:
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -49,6 +49,116 @@ This is the same paragraph on a new line
             ],
         )
     
+    def test_block_to_block_type_heading(self):
+        heading_block = "### Heading"
+        self.assertEqual(
+            block_to_block_type(heading_block),
+            BlockType.HEADING
+        )
+        heading_block = "###### Heading"
+        self.assertEqual(
+            block_to_block_type(heading_block),
+            BlockType.HEADING
+        )
+        not_heading_block = "####### not a heading"
+        self.assertEqual(
+            block_to_block_type(not_heading_block),
+            BlockType.PARAGRAPH
+        )
+        not_heading_block = "##"
+        self.assertEqual(
+            block_to_block_type(not_heading_block),
+            BlockType.PARAGRAPH
+        )
+        not_heading_block = "#Hello"
+        self.assertEqual(
+            block_to_block_type(not_heading_block),
+            BlockType.PARAGRAPH
+        )
+        not_heading_block = "Hello"
+        self.assertEqual(
+            block_to_block_type(not_heading_block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_code(self):
+        code_block = "```\ncode```"
+        self.assertEqual(
+            block_to_block_type(code_block),
+            BlockType.CODE
+        )
+        not_code_block = "```code```"
+        self.assertEqual(
+            block_to_block_type(not_code_block),
+            BlockType.PARAGRAPH
+        )
+        not_code_block = "```\ncode"
+        self.assertEqual(
+            block_to_block_type(not_code_block),
+            BlockType.PARAGRAPH
+        )
+        not_code_block = "code```"
+        self.assertEqual(
+            block_to_block_type(not_code_block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_quote(self):
+        quote_block = "> Hello\n>Hello"
+        self.assertEqual(
+            block_to_block_type(quote_block),
+            BlockType.QUOTE
+        )
+        not_quote_block = "> Hello\nHello"
+        self.assertEqual(
+            block_to_block_type(not_quote_block),
+            BlockType.PARAGRAPH
+        )
+        not_quote_block = " Hello\n>Hello"
+        self.assertEqual(
+            block_to_block_type(not_quote_block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_unordered(self):
+        unordered_block = "- Hello\n- Hello"
+        self.assertEqual(
+            block_to_block_type(unordered_block),
+            BlockType.UNORDERED_LIST
+        )
+        not_unordered_block = "- Hello\n-Hello"
+        self.assertEqual(
+            block_to_block_type(not_unordered_block),
+            BlockType.PARAGRAPH
+        )
+        not_unordered_block = "Hello\n- Hello"
+        self.assertEqual(
+            block_to_block_type(not_unordered_block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_ordered(self):
+        unordered_block = "1. first\n2. second"
+        self.assertEqual(
+            block_to_block_type(unordered_block),
+            BlockType.ORDERED_LIST
+        )
+        unordered_block = "2. first\n3. second"
+        self.assertEqual(
+            block_to_block_type(unordered_block),
+            BlockType.PARAGRAPH
+        )
+        unordered_block = "1.first"
+        self.assertEqual(
+            block_to_block_type(unordered_block),
+            BlockType.PARAGRAPH
+        )
+        unordered_block = "1. first\n3. second"
+        self.assertEqual(
+            block_to_block_type(unordered_block),
+            BlockType.PARAGRAPH
+        )
+        
 
 if __name__ == "__main__":
     unittest.main()
